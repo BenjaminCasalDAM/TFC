2b6fc372911bedf389aa573c642a38a055e795c4
BenjaminCF <118264822+BenjaminCasalDAM@users.noreply.github.com>
2023-05-26 14:43:31 +0200

    Add files via upload

---
+++ InsertBD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFC_Benjamin
+{
+    class InsertBD
+    {
+        string cadenaConexion = "Data Source=.\\sqlexpress;Initial Catalog=master;Integrated Security=True;";
+
+        public void InsertInfoIngrediente(InfoNutricionalIngrediente infoIngrediente)
+        {
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(cadenaConexion))
+                {
+                    conn.Open();
+                    Console.WriteLine("Connection Open!");
+
+                    string insertInfoIngrediente = "USE [InformacionNutricional]; INSERT INTO ValoresNutricionalesIngredientes (IngredienteID, Calorias, ProteinasGrs, Carbohidratos, Grasas) " +
+                                                   "VALUES(40, @kcal, @prot, @carbs, @grasa);";
+
+                    using (SqlCommand comandoInsertInfoIngrediente = new SqlCommand(insertInfoIngrediente, conn))
+                    {
+                        comandoInsertInfoIngrediente.Parameters.AddWithValue("@kcal", infoIngrediente.calorias);
+                        comandoInsertInfoIngrediente.Parameters.AddWithValue("@prot", infoIngrediente.proteinas);
+                        comandoInsertInfoIngrediente.Parameters.AddWithValue("@carbs", infoIngrediente.carbohidratos);
+                        comandoInsertInfoIngrediente.Parameters.AddWithValue("@grasa", infoIngrediente.grasas);
+
+                        Console.WriteLine(infoIngrediente.calorias);
+
+                        comandoInsertInfoIngrediente.ExecuteNonQuery();
+
+                        Console.WriteLine(infoIngrediente.calorias);
+                    }
+
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                Console.WriteLine(sqlex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
+
+namespace TFC_Benjamin
+{
+    class Program
+    {
+        
+
+        static async Task Main(string[] args)
+        {
+            ObtenerDatos datos = new ObtenerDatos();
+            InfoNutricionalIngrediente infoIngrediente = new InfoNutricionalIngrediente();
+            //InsertBD operacionesInsert = new InsertBD();
+            VentanaPrincipal vtaMain = new VentanaPrincipal();
+
+            Application.Run(vtaMain);
+
+            infoIngrediente = await datos.ObtenerValoresAlimento();
+            //operacionesInsert.InsertInfoIngrediente(infoIngrediente);
+
+            
+
+            
+        }
+    }
+}
